8b0f7b16ce386e83186fd807646701a28361346c
Nguyen <huynhbaonguyenqn@gmail.com>
2025-01-22 00:56:58 +0700

    Add TodoDb.cs

---
+++ TodoApi/TodoDb.cs
@@ -0,0 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi;
+
+public class TodoDb : DbContext
+{
+    public TodoDb(DbContextOptions<TodoDb> options)
+        : base(options) { }
+
+    public DbSet<Todo> Todos => Set<Todo>();
+}
